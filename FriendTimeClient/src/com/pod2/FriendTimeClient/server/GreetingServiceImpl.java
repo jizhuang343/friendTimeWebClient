4c4e2f6c67e07450d47e9e44ebb1b2013fa7045e
jizhuang <jizhuang343@gmail.com>
2012-10-05 15:18:27 -0700

    demo to vin and mal

--- FriendTimeClient/src/com/pod2/FriendTimeClient/server/GreetingServiceImpl.java
+++ FriendTimeClient/src/com/pod2/FriendTimeClient/server/GreetingServiceImpl.java
@@ -1,48 +1,39 @@
 package com.pod2.FriendTimeClient.server;
 
-import com.pod2.FriendTimeClient.client.GreetingService;
-import com.pod2.FriendTimeClient.shared.FieldVerifier;
+import java.util.List;
+
+//import com.google.gson.Gson;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.pod2.FriendTimeClient.client.GreetingService;
+import com.pod2.FriendTimeClient.shared.FacebookUtil;
+import com.pod2.FriendTimeClient.shared.Util;
 
 /**
  * The server side implementation of the RPC service.
  */
-@SuppressWarnings("serial")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-		GreetingService {
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
+    private static final long serialVersionUID = 1L;
 
-	public String greetServer(String input) throws IllegalArgumentException {
-		// Verify that the input is valid. 
-//		if (!FieldVerifier.isValidName(input)) {
-//			// If the input is not valid, throw an IllegalArgumentException back to
-//			// the client.
-//			throw new IllegalArgumentException(
-//					"Name must be at least 4 characters long");
-//		}
-//
-		String serverInfo = getServletContext().getServerInfo();
-		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-//
-//		// Escape data from the client to avoid cross-site script vulnerabilities.
-//		input = escapeHtml(input);
-//		userAgent = escapeHtml(userAgent);
+    public String login(final String authToken) {
+        final String url = FacebookUtil.getAccessTokenUrl(Util.encode(authToken));
+        return Util.fetchURL(url);
+    } 
 
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
-	}
+//    public List<FbFriend> findFriends(final String authToken) {
+//        final String friendsUrl = FacebookUtil.getFriendsUrl(encodeUrl(authToken));
+//        final String json = Util.fetchURL(friendsUrl);
+//        final FBFriends fbFriends = new Gson().fromJson(json, FBFriends.class);
+//        return fbFriends.getData();
+//    }
 
-	/**
-	 * Escape an html string. Escaping data received from the client helps to
-	 * prevent cross-site script vulnerabilities.
-	 * 
-	 * @param html the html string to escape
-	 * @return the escaped string
-	 */
-	private String escapeHtml(String html) {
-		if (html == null) {
-			return null;
-		}
-		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;");
-	}
+    private static String encodeUrl(final String unencodedToken) {
+        final String[] parts = unencodedToken.split("=");
+
+        if (parts.length < 2) {
+            return "";
+        }
+
+        return "access_token=" + Util.encode(parts[1]);
+    }
+
 }
